5ffd6d13fe6efc4bcb6867ad7a42dc87d5bc9623
Celine Rolinda <celinerlnda@gmail.com>
2021-10-14 12:39:38 +0800

    resize image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div class="container mt-5" style="max-width: 550px">
+                        <h2 class="mb-5">Laravel Image Resize Example</h2>
+
+                        <form action="{{ route('resizeImage') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            @if ($message = Session::get('success'))
+                                <div class="alert alert-success">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+
+
+                                <div class="col-md-12 mb-3">
+                                    <strong>Grayscale Image:</strong><br />
+                                    <img src="/uploads/{{ Session::get('fileName') }}" width="550px" />
+                                </div>
+                            @endif
+
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
+                            <div class="custom-file">
+                                <input type="file" name="file" class="custom-file-input" id="chooseFile">
+                                <label class="custom-file-label" for="chooseFile">Select file</label>
+                            </div>
+
+                            <button type="submit" name="submit" class="btn btn-outline-danger btn-block mt-4">
+                                Upload
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/file-resize', [ResizeController::class, 'index']);
+Route::post('/resize-file', [ResizeController::class, 'resizeImage'])->name('resizeImage');
